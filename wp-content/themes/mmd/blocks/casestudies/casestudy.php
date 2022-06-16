6cbc25ec21ee5d01c9a99e54f8d360210ecfc3da
Gregor <gspowart@gmail.com>
2022-06-16 11:09:09 +0100

    Various amends from Keira

--- wp-content/themes/mmd/blocks/casestudies/casestudy.php
+++ wp-content/themes/mmd/blocks/casestudies/casestudy.php
@@ -47,22 +47,24 @@ if ($layoutType == "carousel") {
 } else {
 ?>
   <div class="case-studies__study content-area" data-case-study-categories="<?php echo $categories; ?>">
-    <div class="case-studies__image"><img srcset="<?php echo $imgURL; ?> 1x, <?php echo $imgURLx2; ?> 2x" alt="<?php echo $imgAlt; ?>" />
-      <div class="case-studies__details">
-        <h3 class="case-studies__title"><?php echo $title; ?></h3>
-        <p><?php echo $extract; ?></p>
-        <a class="btn btn--default open-modal showCaseStudy" data-case-study-id="1" href="#">Read more</a>
-        <div class="case-studies__details--popup-content glide__slide">
-          <div class="row row--gutters">
-            <div class="row__large-6 row--center-content-vertically content-area">
-              <p class="headline headline--h4 headline--blue headline--uppercase headline--margin-b-mid"><?php echo $type; ?></p>
-              <h3 class="headline headline--h3 headline--underline"><?php echo $title; ?></h3>
-              <?php echo $content; ?>
-            </div>
-            <div class="row__large-6 row--center-content"><img srcset="<?php echo $imgURL; ?> 1x, <?php echo $imgURLx2; ?> 2x" alt="<?php echo $imgAlt; ?>" /></div>
+    <div class="case-studies__image">
+      <a href="#" class="open-modal showCaseStudy"><img srcset="<?php echo $imgURL; ?> 1x, <?php echo $imgURLx2; ?> 2x" alt="<?php echo $imgAlt; ?>" /></a>
+    </div>
+    <div class="case-studies__details">
+      <h3 class="case-studies__title"><?php echo $title; ?></h3>
+      <p><?php echo $extract; ?></p>
+      <a class="btn btn--default open-modal showCaseStudy" data-case-study-id="1" href="#">Read more</a>
+      <div class="case-studies__details--popup-content glide__slide">
+        <div class="row row--gutters">
+          <div class="row__large-6 row--center-content-vertically content-area">
+            <p class="headline headline--h4 headline--blue headline--uppercase headline--margin-b-mid"><?php echo $type; ?></p>
+            <h3 class="headline headline--h3 headline--underline"><?php echo $title; ?></h3>
+            <?php echo $content; ?>
           </div>
+          <div class="row__large-6 row--center-content"><img srcset="<?php echo $imgURL; ?> 1x, <?php echo $imgURLx2; ?> 2x" alt="<?php echo $imgAlt; ?>" /></div>
         </div>
       </div>
+
     </div>
   </div>
 <?php
--- wp-content/themes/mmd/blocks/imagerow.php
+++ wp-content/themes/mmd/blocks/imagerow.php
@@ -6,7 +6,7 @@ if (isset($attributes['layout'])) {
 
 if ($layout == "carousel") { ?>
 
-  <div class="glide-logos" data-transition-speed="7000">
+  <div class="glide-logos content-area--centre" data-transition-speed="7000">
     <div class="glide__track" data-glide-el="track">
       <div class="glide__slides">
         <?php echo $content; ?>
